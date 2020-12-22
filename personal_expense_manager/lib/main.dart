8662833a99af09b990a5b2196908f6482bd0d9de
nikhil <nik.Gunner4life@gmail.com>
2020-12-21 19:31:11 -0800

    Some more work done. Coming along well.

--- personal_expense_manager/lib/main.dart
+++ personal_expense_manager/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +17,15 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: "1", title: "New Shoes", amount: 69.99,
+        date: DateTime.now()),
+    Transaction(id: "2", title: "Groceries", amount: 29.76,
+        date: DateTime.now()),
+    Transaction(id: "3", title: "Other stuff", amount: 19.22,
+        date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +35,10 @@ class MyHomePage extends StatelessWidget {
       body: Container(
         margin: EdgeInsets.all(16),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Container(
               width: double.infinity,
-              height: 50,
               child: Card(
                 color: Colors.blue,
                 child: Text(
@@ -41,12 +51,55 @@ class MyHomePage extends StatelessWidget {
                 elevation: 10,
               ),
             ),
-            Container(
-                width: double.infinity,
-                child: Card(
-                  child: Text("List of transactions"),
-                )
-            )
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          child: Text(
+                            "\$" + tx.amount.toString(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                              fontSize: 20),
+                          ),
+                          decoration: BoxDecoration(border: Border.all(
+                            color: Colors.purple,
+                            width: 2)),
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 15),
+                          padding: EdgeInsets.all(5),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tx.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                                color: Colors.green,
+                                fontStyle: FontStyle.italic
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                            Text(
+                              tx.date.toString(),
+                              style: TextStyle(
+                                fontSize: 8,
+                                color: Colors.grey,
+                              ),
+                              textAlign: TextAlign.left,
+                            ),
+                          ],
+                        )
+                      ],
+                    ));
+              }).toList(),
+              mainAxisAlignment: MainAxisAlignment.end,
+            ),
           ],
         ),
       )
