91add1b9c95141dea629f4e87dcf8c3f83e3064e
nikhil <nik.Gunner4life@gmail.com>
2020-12-21 18:22:58 -0800

    Moved everything inside a parent container.

--- personal_expense_manager/lib/main.dart
+++ personal_expense_manager/lib/main.dart
@@ -22,33 +22,34 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("My Expenses"),
       ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(16),
-            width: double.infinity,
-            height: 50,
-            child: Card(
-              color: Colors.blue,
-              child: Text(
-                "Chart Area",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-              ),
-              elevation: 10,
-            ),
-          ),
-          Container(
-              margin: EdgeInsets.all(16),
+      body: Container(
+        margin: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Container(
               width: double.infinity,
+              height: 50,
               child: Card(
-                child: Text("List of transactions"),
+                color: Colors.blue,
+                child: Text(
+                  "Chart Area",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                elevation: 10,
+              ),
+            ),
+            Container(
+                width: double.infinity,
+                child: Card(
+                  child: Text("List of transactions"),
+                )
             )
-          )
-        ],
-      ),
+          ],
+        ),
+      )
     );
   }
 }
