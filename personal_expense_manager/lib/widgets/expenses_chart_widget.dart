d7aeed040e95125986104ad3a52c6cc6c20e1db5
nikhil <nik.Gunner4life@gmail.com>
2020-12-22 22:45:46 -0800

    Started the chart widget. More tomorrow!

---
+++ personal_expense_manager/lib/widgets/expenses_chart_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
+
+
+class ExpensesChart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+  ExpensesChart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final current = DateTime.now().subtract(Duration(days: index));
+      double sum = 0.0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if ((recentTransactions[i].date.day == current.day) &&
+            (recentTransactions[i].date.month == current.month) &&
+            (recentTransactions[i].date.year == current.year)) {
+          sum += recentTransactions[i].amount;
+        }
+      }
+      return {'day': DateFormat.E().format(current), 'amount': sum};
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+    );
+  }
+}
