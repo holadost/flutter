145128c34e51184cfbb8f0fa724af10ed0cbdf1f
nikhil <nik.Gunner4life@gmail.com>
2020-12-22 20:00:31 -0800

    Refactored the code and made everything into a stateful widget to help maintain states across widgets.

--- personal_expense_manager/lib/main.dart
+++ personal_expense_manager/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/main_transactions_widget.dart';
+import 'package:personal_expense_manager/widgets/add_transaction_widget.dart';
+import 'package:personal_expense_manager/widgets/transactions_widget.dart';
+import './models/transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,45 +18,76 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(id: "1", title: "New Shoes", amount: 69.99,
+        date: DateTime.now()),
+    Transaction(id: "2", title: "Groceries", amount: 29.76,
+        date: DateTime.now()),
+    Transaction(id: "3", title: "Other stuff", amount: 19.22,
+        date: DateTime.now())
+  ];
+
+  void _addNewTransactions(String title, double amount) {
+    final newTx = Transaction(title: title, amount: amount,
+        date: DateTime.now(), id: DateTime.now().toString());
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            child: AddTransactionWidget(_addNewTransactions),
+            onTap: () {},
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Expenses"),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.add), onPressed: () {})],
+        actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: () {})],
       ),
       body: Container(
-        margin: EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: double.infinity,
-                height: 50,
-                child: Card(
-                  color: Colors.blue,
-                  child: Text(
-                    "Chart Area",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
+          margin: EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: Card(
+                    color: Colors.blue,
+                    child: Text(
+                      "Chart Area",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    elevation: 10,
                   ),
-                  elevation: 10,
                 ),
-              ),
-              MainTransactionsWidget(),
-            ],
-          ),
-        )
-      ),
+                TransactionsWidget(_userTransactions)
+              ],
+            ),
+          )),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
--- personal_expense_manager/lib/widgets/add_transaction_widget.dart
+++ personal_expense_manager/lib/widgets/add_transaction_widget.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
-
-class AddTransactionWidget extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class AddTransactionWidget extends StatefulWidget {
   final Function addTx;
   AddTransactionWidget(this.addTx);
+
+  @override
+  _AddTransactionWidgetState createState() => _AddTransactionWidgetState();
+}
+
+class _AddTransactionWidgetState extends State<AddTransactionWidget> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   void submitData() {
     final title = titleController.text;
@@ -14,7 +19,13 @@ class AddTransactionWidget extends StatelessWidget {
       print("Invalid inputs while entering transaction");
       return;
     }
-    addTx(title, amount);
+    // The state object can access the widget whose state it
+    // manages by using the magic 'widget' object allowing us
+    // to access methods/members from the widget.
+    widget.addTx(title, amount);
+    // Close the modal screen after we have finished entering
+    // the transaction.
+    Navigator.of(context).pop();
   }
 
   @override
