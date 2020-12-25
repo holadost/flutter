4f24d91255daeb5e6c0583117ad35f0a323db219
nikhil <nik.Gunner4life@gmail.com>
2020-12-24 17:50:10 -0800

    Added a new widget for chart bars.

---
+++ personal_expense_manager/lib/widgets/chart_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+
+class ChartBarWidget extends StatelessWidget {
+  final String label;
+  final double amount;
+  final double spendPct;
+
+  ChartBarWidget(this.label, this.amount, this.spendPct);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('\$${spendPct.toStringAsFixed(0)}'),
+        SizedBox(height: 4,),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendPct,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              )
+            ],
+          )
+        )
+      ],
+    );
+  }
+}
--- personal_expense_manager/lib/widgets/expenses_chart_widget.dart
+++ personal_expense_manager/lib/widgets/expenses_chart_widget.dart
@@ -26,6 +26,12 @@ class ExpensesChart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
+      margin: EdgeInsets.all(10),
+      child: Row(
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
+      ),
     );
   }
 }
