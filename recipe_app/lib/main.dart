38da6e761cad78417e232e9aa2ed5d3846362850
nikhil <nik.Gunner4life@gmail.com>
2020-12-29 22:09:34 -0800

    Added more routing functions.

--- recipe_app/lib/main.dart
+++ recipe_app/lib/main.dart
@@ -36,13 +36,35 @@ class MyApp extends StatelessWidget {
           )
         )
       ),
+      // Routes are used to define the various routes/screens/endpoints in
+      // our app. The home attribute can be used to define the home screen.
+      // The home screen can likewise also be defined in the routes attribute
+      // as well. On generate route can be used when we have dynamic routes.
+      // Flutter first checks routes and if no route exists, it executes
+      // onGenerateRoute and returns a screen if any. Otherwise, it returns
+      // onUnknownRoute(if defined). If not defined, the app crashes.
       // home: CategoriesScreen(),
+      //
       routes: {
         // Instead of defining a home attribute, we can directly
         // define the "/" which refers to home.
         "/": (ctx) => CategoriesScreen(),
         CategoryScreen.routeName: (ctx) => CategoryScreen(),
         MealScreen.routeName: (ctx) => MealScreen()
+      },
+      onGenerateRoute: (settings) {
+        print("Checking onGenerateRoute: ${settings.name}");
+        return MaterialPageRoute(
+            builder: (ctx) {
+              return CategoriesScreen();
+            });
+      },
+      onUnknownRoute: (settings) {
+        print("Undefined route: ${settings.name}");
+        return MaterialPageRoute(
+            builder: (ctx) {
+              return CategoriesScreen();
+            });
       },
     );
   }
