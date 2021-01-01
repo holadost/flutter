0faf90bcde882658b142b03a57145ab189dfa58c
nikhil <nik.Gunner4life@gmail.com>
2020-12-31 18:01:05 -0800

    We now populate the product screen using the info given by the provider.

--- rainforest/lib/screens/product_screen.dart
+++ rainforest/lib/screens/product_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rainforest/providers/products_provider.dart';
 
 import '../models/product.dart';
 
@@ -20,7 +22,19 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = ModalRoute.of(context).settings.arguments as Product;
+    final passedProduct = ModalRoute.of(context).settings.arguments as Product;
+    final products = Provider.of<ProductsProvider>(context).items;
+    final Product product = products.firstWhere((prd) {
+      if (prd.id == passedProduct.id) {
+        return true;
+      }
+      return false;
+    });
+    if (product == null) {
+      print("Unable to find the given product");
+      return Text("Unable to find the product");
+    }
+
     return Scaffold(
       appBar: _buildAppBar(context, product),
       body: _buildBody(context, product),
