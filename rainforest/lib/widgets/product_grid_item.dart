a5dad4435bd2719b57b4ace4c0380044588a0a18
nikhil <nik.Gunner4life@gmail.com>
2020-12-31 16:36:48 -0800

    Product grid item is now ready!

--- rainforest/lib/widgets/product_grid_item.dart
+++ rainforest/lib/widgets/product_grid_item.dart
@@ -10,20 +10,25 @@ class ProductGridItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(
-        product.imageURL,
-        fit: BoxFit.cover,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10.0),
+        child: Image.network(
+          product.imageURL,
+          fit: BoxFit.cover,
+        ),
       ),
       footer: GridTileBar(
         leading: IconButton(
           icon: Icon(Icons.favorite),
           onPressed: () {},
+          color: Theme.of(context).accentColor,
         ),
         trailing: IconButton(
           icon: Icon(Icons.shopping_cart),
           onPressed: () {},
+          color: Theme.of(context).accentColor,
         ),
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.black87,
         title: Text(
           product.title,
           textAlign: TextAlign.center,
