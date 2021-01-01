7b75bf59ae201df6bfa1b8e69b7c81a2bb3377d0
nikhil <nik.Gunner4life@gmail.com>
2020-12-31 17:40:38 -0800

    Renamed file.

--- rainforest/lib/main.dart
+++ rainforest/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_screen.dart';
-import './providers/product_provider.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
